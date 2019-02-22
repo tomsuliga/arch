0c603a8895bf50945854c930275a4e7daf71893f
“tom” <tom2wheeler@gmail.com>
2019-02-22 08:30:22 -0700

    Initial boilerplate java spring project

---
+++ src/test/java/org/suliga/arch/ArchApplicationTests.java
@@ -0,0 +1,16 @@
+package org.suliga.arch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
